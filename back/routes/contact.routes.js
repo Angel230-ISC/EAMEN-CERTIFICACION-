320631a9352cd8e350825c8f8d89a933ed7bb2c7
Barragan11 <158596646+Barragan11@users.noreply.github.com>
2025-11-04 09:39:00 -0600

    Delete back directory

--- back/routes/contact.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { enviarMensaje } = require("../controllers/contact.controller");
-const router = express.Router();
-
-router.post("/contacto", enviarMensaje);
-
-module.exports = router;
